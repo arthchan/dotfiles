d9b5e7feb0b1f05a777037b197aef1d886d5f453
Arthur Chan <64251502+arthchan@users.noreply.github.com>
2024-12-01 16:59:44 +0800

    Modify descriptions

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -9,8 +9,8 @@ return {
 	},
 	keys = {
 		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", desc = "Tmux: Navigate to the left pane" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Tmux: Navigate to the below pane" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Tmux: Navigate to the above pane" },
+		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Tmux: Navigate to the bottom pane" },
+		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Tmux: Navigate to the top pane" },
 		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Tmux: Navigate to the right pane" },
 		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Tmux: Navigate to the previous pane" },
 	},

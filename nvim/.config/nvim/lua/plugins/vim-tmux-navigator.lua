e6b4192bfd92a93ff2adbd79535e04ae054e2283
Arthur Chan <64251502+arthchan@users.noreply.github.com>
2024-12-01 16:08:30 +0800

    Add descriptions for keymaps

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -8,10 +8,10 @@ return {
 		"TmuxNavigatePrevious",
 	},
 	keys = {
-		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", desc = "Tmux: Navigate to the left pane" },
+		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Tmux: Navigate to the below pane" },
+		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Tmux: Navigate to the above pane" },
+		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Tmux: Navigate to the right pane" },
+		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Tmux: Navigate to the previous pane" },
 	},
 }

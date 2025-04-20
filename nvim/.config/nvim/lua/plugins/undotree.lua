1dad3d058eaa10db898be3d785322b2b8d021788
Arthur Chan <64251502+arthchan@users.noreply.github.com>
2025-04-20 12:11:12 +0800

    Set to use FC on Windows OS

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -1,6 +1,7 @@
 return {
 	"mbbill/undotree",
 	config = function()
+		if vim.fn.has('win32') then vim.g.undotree_DiffCommand = "FC" end
 		vim.keymap.set("n", "<C-u>", vim.cmd.UndotreeToggle, { desc = "Undotree: Toggle Undotree" })
 	end,
 }
